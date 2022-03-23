1105b9b24ed75d4dc9d2fb4eeb7db2f12261b90e
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2022-03-23 14:24:26 +0800

    Week 6 add unsharp mask

--- week4.py
+++ week4.py
@@ -3,68 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def show_gray_histogram(img):
-    # -- Show Histogram
-    cv.imshow('original', img)
-    hist = cv.calcHist([img], [0], None, [256], [0, 256])
-    plt.plot(hist)
-    # ravel 將多維陣列拆開成為一維
-    plt.hist(img.ravel(), 256, [0, 256])
-    plt.show()
-
-
-def show_color_histogram(img):
-    # -- 畫出彩色影像的直方圖
-    # cv.imshow(description, img)
-    cv.imshow('original', img)
-    color = ('b', 'g', 'r')
-    for i, col in enumerate(color):
-        hist = cv.calcHist([img], [i], None, [256], [0, 256])
-        plt.plot(hist, color=col)
-        plt.xlim([0, 256])
-    plt.show()
-    pass
-
-
-def show_histogram_with_subplot(img):
-    # -- Show Histogram with subplot
-    # 產生一個遮罩
-    mask = np.zeros(img.shape[:2], np.uint8)
-    mask[100:300, 100:400] = 255
-    masked_img = cv.bitwise_and(img, img, mask=mask)
-    hist_full = cv.calcHist([img], [0], None, [256], [0, 256])
-    hist_mask = cv.calcHist([img], [0], mask, [256], [0, 256])
-    plt.subplot(2, 2, 1)
-    plt.imshow(img, 'gray')
-    plt.subplot(2, 2, 2)
-    plt.imshow(mask, 'gray')
-    plt.subplot(2, 2, 3)
-    plt.imshow(masked_img, 'gray')
-    plt.subplot(2, 2, 4)
-    plt.plot(hist_full)
-    plt.plot(hist_mask)
-    plt.xlim([0, 256])
-    plt.show()
-
-
-def opencv_histogram_equalization(img):
-    # -- Histogram Equalization with OpenCV
-    cv.imshow('Original image', img)
-    hist_ori = cv.calcHist([img], [0], None, [256], [0, 256])
-    plt.figure(1)
-    # plt.plot(hist_ori)
-    plt.hist(img.ravel(), 256, [0, 256])  # ravel 將多維陣列拆開成為一維
-
-    img_eq = cv.equalizeHist(img)
-    cv.imshow('Equalized image', img_eq)
-    hist_eq = cv.calcHist([img_eq], [0], None, [256], [0, 256])
-    # plt.plot(hist_eq)
-    plt.hist(img_eq.ravel(), 256, [0, 256])
-    plt.show()
-
-
-def image_enhance(img):
-    cv.imshow('original', img)
+def image_contrast_enhance(img):
+    cv.imshow('gray image', img)
     new_image = np.zeros(img.shape, img.dtype)
     alpha = 1.0
     beta = 50
@@ -73,24 +13,73 @@ def image_enhance(img):
         for x in range(img.shape[1]):
             for c in range(img.shape[2]):
                 # new_image[y, x, c] = np.clip(alpha * img[y, x, c] + beta, 0, 255)
-                new_image[y, x, c] = np.clip(pow((img[y, x, c] / 255.0), gamma) * 255.0, 0, 255)
-    cv.imshow('Enhanced', new_image)
+                new_image[y, x, c] = np.clip(pow(img[y, x, c] / 255.0, gamma) * 255.0, 0, 255)
+    cv.imshow('Contrast enhanced', new_image)
 
 
-# Main
+def show_gray_histogram(img):
+    cv.imshow('original gray image', img)
+    # hist = cv.calcHist([img], [0], None, [256], [0, 256])
+    # plt.plot(hist)
+    plt.hist(img.ravel(), 256, [0, 256])
+    plt.show()
+    pass
+
+
+def show_color_histogram(img):
+    cv.imshow('original color image', img)
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        hist = cv.calcHist([img], [i], None, [256], [0, 256])
+        plt.plot(hist, color=col)
+        plt.xlim([0, 256])
+    plt.show()
+
+
+def show_histogram_with_subplot(img):
+    cv.imshow('original gray image', img)
+    mask = np.zeros(img.shape[:2], np.uint8)
+    mask[100:300, 200:500] = 255
+    masked_image = cv.bitwise_and(img, img, mask=mask)
+    cv.imshow('masked image', masked_image)
+    hist_full = cv.calcHist([img], [0], None, [256], [0, 256])
+    hist_mask = cv.calcHist([img], [0], mask, [256], [0, 256])
+    plt.subplot(2, 2, 1)
+    plt.imshow(img, 'gray')
+    plt.subplot(2, 2, 2)
+    plt.imshow(mask, 'gray')
+    plt.subplot(2, 2, 3)
+    plt.imshow(masked_image, 'gray')
+    plt.subplot(2, 2, 4)
+    plt.plot(hist_full)
+    plt.plot(hist_mask)
+    plt.xlim([0, 256])
+    plt.show()
+
+
+def opencv_histogram_equalization(img):
+    cv.imshow('original image', img)
+    # hist_ori = cv.calcHist([img], [0], None, [256], [0, 256])
+    plt.figure(1)
+    # plt.plot(hist_ori)
+    plt.hist(img.ravel(), 256, [0, 256])
+
+    img_eq = cv.equalizeHist(img)
+    cv.imshow('equalized image', img_eq)
+    plt.hist(img_eq.ravel(), 256, [0, 256])
+    plt.show()
+
+
 # 讀取影像
-# print(__name__)
 img_ori = cv.imread('image/1233.png')
 img_gray = cv.cvtColor(img_ori, cv.COLOR_BGR2GRAY)
+# cv.imshow('original image', img_ori)
+# cv.imshow('gray image', img_gray)
 
-image_enhance(img_ori)
-
+# image_contrast_enhance(img_gray)
+# image_contrast_enhance(img_ori)
 # show_gray_histogram(img_gray)
-
 # show_color_histogram(img_ori)
-
 # show_histogram_with_subplot(img_gray)
-
-# opencv_histogram_equalization(img_gray)
-
+opencv_histogram_equalization(img_gray)
 cv.waitKey()
--- week5.py
+++ week5.py
@@ -4,111 +4,118 @@ import numpy as np
 
 
 def my_first_filter(img):
-    cv.imshow('original image', img)
-    # 定義一個平均濾波器
-    # kernel = np.ones((5, 5), np.float32) / 25
-    # kernel = np.array([
-    #     [1, 1, 1],
-    #     [1, 1, 1],
-    #     [1, 1, 1]
-    # ]) / 9
-    # 使用filter2d()來進行濾波
+    # # 定義一個平均濾波器
+    kernel = np.ones((7, 7), np.float32) / 49
+    # kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]) /9
 
     # 定義一個edge detection filter
-    # kernel = np.array([
-    #     [-1, -1, -1],
-    #     [-1, 8, -1],
-    #     [-1, -1, -1]
-    # ])
-
-    # Emboss filter
-    # kernel = np.array([
-    #     [-2, -1, 0],
-    #     [-1, 1, 1],
-    #     [0, 1, 2]
-    # ])
-
-    # Sobel filter
-    kernel = np.array([
-        [-1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1]
+    kernel1 = np.array([
+        [-1, -1, -1],
+        [-1, 8, -1],
+        [-1, -1, -1]
     ])
 
-    result = cv.filter2D(img, -1, kernel)
-    cv.imshow('Filtered_image', result)
+    # Emboss filter
+    kernel2 = np.array([
+        [-2, -1, 0],
+        [-1, 1, 1],
+        [0, 1, 2]
+    ])
+
+    img_result = cv.filter2D(img, -1, kernel)
+    img_result1 = cv.filter2D(img, -1, kernel1)
+    img_result2 = cv.filter2D(img, -1, kernel2)
+    cv.imshow('original image', img)
+    cv.imshow('result image', img_result)
+    cv.imshow('edge detection image', img_result1)
+    cv.imshow('emboss image', img_result2)
 
 
 def averaging_filter(img):
     cv.imshow('original image', img)
-
-    # averaging blur filter
-    img_averaging = cv.blur(img, (11, 11))
+    img_averaging = cv.blur(img, (5, 5))
     cv.imshow('blur image', img_averaging)
 
 
+def median_filter(img):
+    cv.imshow('original image', img)
+    img_median = cv.medianBlur(img, 7)
+    cv.imshow('median blur image', img_median)
+
+
 def gaussian_filter(img):
-    # Gaussian blur filter
+    cv.imshow('original image', img)
     img_gaussian_blur = cv.GaussianBlur(img, (11, 11), -1)
     cv.imshow('Gaussian blur image', img_gaussian_blur)
 
 
-def median_filter(img):
-    # median filter
-    img_median = cv.medianBlur(img, 11)
-    cv.imshow('median blur image', img_median)
+def laplacian_filter(img):
+    cv.imshow('original image', img)
+    gray_lap = cv.Laplacian(img, cv2.CV_16S, ksize=5)
+    img_laplacian = cv.convertScaleAbs(gray_lap)
+    cv.imshow('Laplacian image', img_laplacian)
 
 
 def sobel_filter(img):
-    # Sobel filter
+    cv.imshow('original image', img)
     x = cv.Sobel(img, cv2.CV_16S, 1, 0)
     y = cv.Sobel(img, cv2.CV_16S, 0, 1)
-
-    abs_x = cv.convertScaleAbs(x)  # 轉回uint8
+    abs_x = cv.convertScaleAbs(x)
     abs_y = cv.convertScaleAbs(y)
     img_sobel = cv.addWeighted(abs_x, 0.5, abs_y, 0.5, 0)
 
     cv.imshow('x-direction gradient image', abs_x)
     cv.imshow('y-direction gradient image', abs_y)
-    cv.imshow('Sobel image', img_sobel)
+    cv.imshow('sobel image', img_sobel)
 
 
-def laplacian_filter(img):
-    # Laplacian filter
-    gray_lap = cv2.Laplacian(img, cv2.CV_16S, ksize=3)
-    img_laplacian = cv2.convertScaleAbs(gray_lap)  # 轉回uint8
-    cv.imshow('Laplacian image', img_laplacian)
-
-
-def add_gaussian_noise(img, mean=0, sigma=0.1):
+def add_gaussian_noise(img):
     cv.imshow('original image', img)
-    # int -> float (標準化)
     img = img / 255
-    # 隨機生成高斯 noise (float + float)
+    mean = 0
+    sigma = 0.1
     noise = np.random.normal(mean, sigma, img.shape)
-    # noise + 原圖
     img_gaussian = img + noise
-    # 所有值必須介於 0~1 之間，超過1 = 1，小於0 = 0
     img_gaussian = np.clip(img_gaussian, 0, 1)
-    # 原圖: float -> int (0~1 -> 0~255)
     img_gaussian = np.uint8(img_gaussian * 255)
-    # noise: float -> int (0~1 -> 0~255)
     noise = np.uint8(noise * 255)
     cv.imshow('Gaussian noise', noise)
-    cv.imshow('noise image', img_gaussian)
+    cv.imshow('noised image', img_gaussian)
+    median_filter(img_gaussian)
 
-    dst = cv2.fastNlMeansDenoising(img_gaussian, None, 10, 10, 7)
-    cv.imshow('denoise', dst)
-    # median_filter(img_gaussian)
+    img_result = cv.fastNlMeansDenoising(img_gaussian, None, 10, 10, 7)
+    cv.imshow('fast denoise', img_result)
 
 
-#     return gaussian_out , noise# 讀取影像
+def unsharp_mask(img, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    # For details on unsharp masking, see:
+    # https://en.wikipedia.org/wiki/Unsharp_masking
+    # https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm
+    cv.imshow('original image', img)
+
+    blurred = cv.GaussianBlur(img, kernel_size, sigma)
+    cv.imshow('blurred image', blurred)
+
+    sharpened = float(amount + 1) * img - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(img - blurred) < threshold
+        np.copyto(sharpened, img, where=low_contrast_mask)
+    cv.imshow('sharpened image', sharpened)
+
+
+# reading original image
 img_ori = cv.imread('image/cameraman.png')
+# img_ori = cv.imread('image/gray.png')
 # my_first_filter(img_ori)
 # averaging_filter(img_ori)
-# gaussian_filter(img_ori)
 # median_filter(img_ori)
-# sobel_filter(img_ori)
+# gaussian_filter(img_ori)
 # laplacian_filter(img_ori)
-add_gaussian_noise(img_ori)
+# sobel_filter(img_ori)
+# add_gaussian_noise(img_ori)
+unsharp_mask(img_ori)
 cv.waitKey()
