e6dddaed4523cdd607acedfa744fc3f729b3415e
陳定宏 <chendh@office.stust.edu.tw>
2022-05-11 23:16:17 +0800

    morphology operations

--- my_modules.py
+++ my_modules.py
@@ -294,40 +294,39 @@ class MyFunction:
                 return cv.MORPH_ELLIPSE
 
         def erosion(val):
-            erosion_size = cv.getTrackbarPos(title_trackbar_kernel_size, title_erosion_window)
-            erosion_shape = morph_shape(cv.getTrackbarPos(title_trackbar_element_shape, title_erosion_window))
+            erosion_size = cv.getTrackbarPos(kernel_size, erosion_window)
+            erosion_shape = morph_shape(cv.getTrackbarPos(element_shape, erosion_window))
 
             element = cv.getStructuringElement(erosion_shape, (2 * erosion_size + 1, 2 * erosion_size + 1),
                                                (erosion_size, erosion_size))
 
             erosion_dst = cv.erode(src, element)
-            cv.imshow(title_erosion_window, erosion_dst)
+            cv.imshow(erosion_window, erosion_dst)
 
         def dilatation(val):
-            dilatation_size = cv.getTrackbarPos(title_trackbar_kernel_size, title_dilation_window)
-            dilation_shape = morph_shape(cv.getTrackbarPos(title_trackbar_element_shape, title_dilation_window))
+            dilatation_size = cv.getTrackbarPos(kernel_size, dilation_window)
+            dilation_shape = morph_shape(cv.getTrackbarPos(element_shape, dilation_window))
             element = cv.getStructuringElement(dilation_shape, (2 * dilatation_size + 1, 2 * dilatation_size + 1),
                                                (dilatation_size, dilatation_size))
             dilatation_dst = cv.dilate(src, element)
-            cv.imshow(title_dilation_window, dilatation_dst)
+            cv.imshow(dilation_window, dilatation_dst)
 
-        erosion_size = 0
         max_elem = 2
         max_kernel_size = 21
-        title_trackbar_element_shape = 'Element:\n 0: Rect \n 1: Cross \n 2: Ellipse'
-        title_trackbar_kernel_size = 'Kernel size:\n 2n +1'
-        title_erosion_window = 'Erosion Demo'
-        title_dilation_window = 'Dilation Demo'
+        element_shape = 'Element:\n 0: Rect \n 1: Cross \n 2: Ellipse'
+        kernel_size = 'Kernel size:\n 2n +1'
+        erosion_window = 'Erosion Demo'
+        dilation_window = 'Dilation Demo'
 
         src = self.open_file()
         cv.imshow('Source', src)
         # img_thresh = add_gaussian_noise(src)
-        cv.namedWindow(title_erosion_window)
-        cv.createTrackbar(title_trackbar_element_shape, title_erosion_window, 0, max_elem, erosion)
-        cv.createTrackbar(title_trackbar_kernel_size, title_erosion_window, 0, max_kernel_size, erosion)
-        cv.namedWindow(title_dilation_window)
-        cv.createTrackbar(title_trackbar_element_shape, title_dilation_window, 0, max_elem, dilatation)
-        cv.createTrackbar(title_trackbar_kernel_size, title_dilation_window, 0, max_kernel_size, dilatation)
+        cv.namedWindow(erosion_window)
+        cv.createTrackbar(element_shape, erosion_window, 0, max_elem, erosion)
+        cv.createTrackbar(kernel_size, erosion_window, 0, max_kernel_size, erosion)
+        cv.namedWindow(dilation_window)
+        cv.createTrackbar(element_shape, dilation_window, 0, max_elem, dilatation)
+        cv.createTrackbar(kernel_size, dilation_window, 0, max_kernel_size, dilatation)
         erosion(0)
         dilatation(0)
 
